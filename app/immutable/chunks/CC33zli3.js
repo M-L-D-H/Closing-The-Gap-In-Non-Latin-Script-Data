c6212c4edf116a47c5052dad88034aea97bcd5b4
Kudela <kudem94@fu-berlin.de>
2025-05-22 15:26:03 +0200

    Updates

---
+++ app/immutable/chunks/CC33zli3.js
@@ -0,0 +1,1 @@
+import{s as e}from"./DXI4-2_c.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
---
+++ app/immutable/entry/start.6VFSwPbX.js
@@ -0,0 +1,1 @@
+import{l as o,c as r}from"../chunks/DXI4-2_c.js";export{o as load_css,r as start};
