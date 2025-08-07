80d227c60563ba5b09a65a5b439471972f1ddfba
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-08-07 17:52:51 -0400

    Updates

---
+++ app/immutable/nodes/7.DRPeUtXV.js
@@ -0,0 +1,23 @@
+import{_ as j}from"../chunks/C1FmrZbK.js";import{S as E,i as k,s as D,n as u,d as c,b as g,c as P,m as _,o as S,h as F,e as x,f as v,k as $,j as w,D as I,E as L,F as T}from"../chunks/BA6SDppQ.js";import"../chunks/IHki7fMi.js";import{f as C}from"../chunks/CEOVDL7U.js";import{e as M}from"../chunks/DVsh0hJ0.js";function O(p){let n,o,e;return{c(){n=$(),o=w("div"),e=w("div"),this.h()},l(t){S("svelte-1w6dn8w",document.head).forEach(c),n=F(t),o=x(t,"DIV",{class:!0});var d=v(o);e=x(d,"DIV",{class:!0}),v(e).forEach(c),d.forEach(c),this.h()},h(){document.title="Closing the Gap in Non-Latin-Script Data – Map",_(e,"class","h-[34rem] rounded-md font-normal"),_(o,"class","mx-auto max-w-6xl px-4")},m(t,a){g(t,n,a),g(t,o,a),P(o,e),p[1](e)},p:u,i:u,o:u,d(t){t&&(c(n),c(o)),p[1](null)}}}const R="https://github.com/M-L-D-H/Closing-The-Gap-In-Non-Latin-Script-Data/blob/master/PROJECTS/";function V(p,n,o){let e,t;const a={};I(async()=>{const r=await j(()=>import("../chunks/DTmlu4rB.js").then(s=>s.l),[],import.meta.url);t=r.map(e).setView([51.53443,9.93228],5),r.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(t);const b=M;for(const[s,i]of b){const h=C(i.project.places),l=`${R}${s.split("/PROJECTS/")[1]}`;for(const m of h){const f=m.coordinates.lat.split(".")[0]+m.coordinates.lng.split(".")[0];if(a[f])a[f].projects.push({title:i.project.title,url:l});else{const y={lat:m.coordinates.lat,lng:m.coordinates.lng,projects:[{title:i.project.title,url:l}]};a[f]=y}}}for(const[,s]of Object.entries(a)){const i=s.projects.map(l=>`<a
+							href=${l.url}
+							target="_blank"
+							rel="noreferrer"
+							class="font-fira">${l.title}</a
+						>`).join('<hr class="my-1.5" />'),h=r.divIcon({html:`
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 48">
+					<path
+						d="m1 16 15 30 15-30a8 8 0 0 0-30 0Z"
+						stroke-width="2"
+						stroke="#06F"
+						fill="#06F"
+						fill-opacity=".4"
+					/>
+					<circle cx="16" cy="16" r="10" fill="#FFF" stroke="#06F" />
+					<text
+						text-anchor="middle"
+						x="16"
+						y="20"
+						fill="rgba(0, 0, 0,1)">${s.projects.length}</text
+					>
+				</svg>
+				`,className:"font-fira text-[13px]",iconSize:[32,48],iconAnchor:[16,48]});r.marker([s.lat,s.lng],{icon:h}).addTo(t).bindPopup(i,{maxHeight:250,maxWidth:250})}}),L(()=>{t&&t.remove()});function d(r){T[r?"unshift":"push"](()=>{e=r,o(0,e)})}return[e,d]}class J extends E{constructor(n){super(),k(this,n,V,O,D,{})}}export{J as component};
